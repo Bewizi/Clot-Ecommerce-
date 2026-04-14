44105ba015de93114c02251657fcbc61e5a88402
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-04-14 17:46:27 +0100

    feat: implement ForgotPassword widget
    - Create `ForgotPassword` widget in `lib/features/auth/presentation/pages/forgot_password/forgot_password.dart`.
    - Define static `routeName` for navigation.
    - Implement UI layout using `AppScaffold`, `AppTextField` for email input, and a `PrimaryButton`.
    - Include form validation for required email field and basic email format.

---
+++ lib/features/auth/presentation/pages/forgot_password/forgot_password.dart
@@ -0,0 +1,72 @@
+import 'package:clot/core/ui/components/app_back_button.dart';
+import 'package:clot/core/ui/components/app_button.dart';
+import 'package:clot/core/ui/components/app_text.dart';
+import 'package:clot/core/ui/components/app_text_field.dart';
+import 'package:clot/core/ui/components/layouts/app_scaffold.dart';
+import 'package:clot/core/ui/extensions/app_spacing_extension.dart';
+import 'package:clot/core/ui/extensions/app_theme_extension.dart';
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
+  static const String routeName = '/forgot-password';
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScaffold(
+      appbar: AppBar(
+        leading: const AppBackButton(),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AppText(
+            'Forgot Password',
+            style: context.textTheme.headlineLarge,
+          ),
+          32.verticalSpacing,
+          Form(
+            key: formKey,
+            child: Column(
+              children: [
+                AppTextField(
+                  hintText: 'Email Address',
+                  controller: emailController,
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email address';
+                    }
+                    if (!value.contains('@')) {
+                      return 'Please enter a valid email address';
+                    }
+                    return null;
+                  },
+                ),
+
+                32.verticalSpacing,
+                PrimaryButton(
+                  pressed: () {
+                    if (formKey.currentState!.validate()) {
+                      // TODO: Sign in user
+                    }
+                  },
+                  'Continue',
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
