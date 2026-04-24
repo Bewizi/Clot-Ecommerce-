3d2d9f8c3b9f4389e471b9100bdc1f6a90e86883
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-04-24 16:39:30 +0100

    feat: update image asset constants in AppImages
    - Remove `kFaceBookIcon` and `kGoogleIcon` constants from `lib/core/variables/app_images.dart`.
    - Add `kEMailBox` constant pointing to the email box image asset.

--- lib/core/variables/app_images.dart
+++ lib/core/variables/app_images.dart
@@ -1,6 +1,5 @@
 class AppImages {
   static const String kLogo = 'assets/images/logo.png';
 
-  static const String kFaceBookIcon = 'assets/images/facebook_svg.png';
-  static const String kGoogleIcon = 'assets/images/google_svg.png';
+  static const String kEMailBox = 'assets/images/email_box.png';
 }
