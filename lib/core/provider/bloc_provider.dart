071fd360a484c547996cb344011c670cd1491d97
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2026-05-06 23:55:23 +0100

    feat: register CategoriesBloc in appBlocProvider
    - Add `BlocProvider<CategoriesBloc>` to the `appBlocProvider` list in `lib/core/provider/bloc_provider.dart`.
    - Initialize `CategoriesBloc` with `CategoriesDataImpl` repository.

--- lib/core/provider/bloc_provider.dart
+++ lib/core/provider/bloc_provider.dart
@@ -1,9 +1,15 @@
 import 'package:clot/features/auth/data/auth_data.dart';
 import 'package:clot/features/auth/presentation/bloc/bloc/auth_bloc.dart';
+import 'package:clot/features/home/data/categories_data.dart';
+import 'package:clot/features/home/presentation/bloc/categories_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 final List<BlocProvider> appBlocProvider = [
   BlocProvider<AuthBloc>(
     create: (context) => AuthBloc(AuthData()),
+  ),
+  BlocProvider<CategoriesBloc>(
+    create: (context) =>
+        CategoriesBloc(categoriesRepository: CategoriesDataImpl()),
   ),
 ];
