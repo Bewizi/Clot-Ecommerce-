295c9186910f0964195d36ec062b0b6cadea19fa
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2026-05-09 15:05:07 +0100

    feat: expand ProductsRepository with new retrieval methods
    - Uncomment `getAllProducts` in `lib/features/products/domain/products_repository.dart`.
    - Add `getTopSellingProducts` and `getNewInProducts` methods to the `ProductsRepository` interface.

--- lib/features/products/domain/products_repository.dart
+++ lib/features/products/domain/products_repository.dart
@@ -2,6 +2,8 @@ import 'package:clot/features/products/domain/products_domain.dart';
 
 abstract class ProductsRepository {
   Future<List<ProductsDomain>> getProductsByCategoryId(String categoryId);
-  // Future<List<ProductsDomain>> getAllProducts();
+  Future<List<ProductsDomain>> getAllProducts();
+  Future<List<ProductsDomain>> getTopSellingProducts();
+  Future<List<ProductsDomain>> getNewInProducts();
   // Future<ProductsDomain> getProductById(String productId);
 }
