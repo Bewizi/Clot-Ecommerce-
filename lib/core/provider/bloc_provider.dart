d1c14e60d4afe31115688cc84b6f83de618af458
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2026-05-12 12:57:48 +0100

    feat: register CartBloc in bloc_provider
    - Import `CartDataImpl` and `CartBloc` in `lib/core/provider/bloc_provider.dart`.
    - Add `BlocProvider<CartBloc>` to the provider list, initializing it with `CartDataImpl`.

--- lib/core/provider/bloc_provider.dart
+++ lib/core/provider/bloc_provider.dart
@@ -1,5 +1,7 @@
 import 'package:clot/features/auth/data/auth_data.dart';
 import 'package:clot/features/auth/presentation/bloc/bloc/auth_bloc.dart';
+import 'package:clot/features/cart/data/cart_data.dart';
+import 'package:clot/features/cart/presentation/bloc/cart_bloc.dart';
 import 'package:clot/features/home/data/categories_data.dart';
 import 'package:clot/features/home/presentation/bloc/categories_bloc.dart';
 import 'package:clot/features/products/bloc/products_bloc.dart';
@@ -16,5 +18,8 @@ final List<BlocProvider> appBlocProvider = [
   ),
   BlocProvider<ProductsBloc>(
     create: (context) => ProductsBloc(productsRepository: ProductsDataImpl()),
+  ),
+  BlocProvider<CartBloc>(
+    create: (context) => CartBloc(cartRepository: CartDataImpl()),
   ),
 ];
