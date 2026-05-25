d3dd8a3686da5649e290dc6fd8064adf41cad033
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2026-05-26 00:09:08 +0100

    feat: integrate order placement functionality in CheckoutPage
    - Add `OrdersBloc` listener for real-time order placement state management.
    - Enable order submission with shipping address, phone, and cart details.
    - Add form validation and input controllers for address and phone fields.
    - Update UI to handle order success or error scenarios.

--- lib/features/cart/presentation/pages/checkout_page.dart
+++ lib/features/cart/presentation/pages/checkout_page.dart
@@ -8,6 +8,7 @@ import 'package:clot/core/ui/extensions/app_color_extension.dart';
 import 'package:clot/core/ui/extensions/app_spacing_extension.dart';
 import 'package:clot/core/variables/colors.dart';
 import 'package:clot/features/cart/presentation/bloc/cart_bloc.dart';
+import 'package:clot/features/order/presentation/bloc/orders_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -19,77 +20,107 @@ class CheckoutPage extends StatefulWidget {
 }
 
 class _CheckoutPageState extends State<CheckoutPage> {
+  final _addressController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _addressController.dispose();
+    _phoneController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AppScaffold(
-      appbar: AppBar(
-        title: AppText(
-          'Checkout',
-          style: appAltTextTheme.bodyMedium?.copyWith(
-            fontWeight: FontWeight.w800,
-            color: Theme.of(context).colorScheme.appText,
-          ),
-        ),
-        centerTitle: true,
-        forceMaterialTransparency: true,
-        leading: const AppBackButton(),
-      ),
-      body: Column(
-        children: [
-          Form(
-            child: Column(
-              children: [
-                // Shipping Address
-                Container(
-                  padding: const EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.bgColor,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: AppTextField(
-                    title: 'Shipping Address',
-                    verticalSpacing: 0,
-                    inputDecoration: InputDecoration(
-                      filled: false,
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.zero,
-                      hintText: 'Add Shipping Address',
-                      hintStyle: TextStyle(
-                        fontSize: appTextTheme.bodyLarge?.fontSize,
-                        color: Theme.of(context).colorScheme.appText,
-                      ),
-                    ),
-                  ),
-                ),
-                16.verticalSpacing,
-                // Payment Method
-                Container(
-                  padding: const EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.bgColor,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: AppTextField(
-                    title: 'Payment Method',
-                    verticalSpacing: 0,
-                    inputDecoration: InputDecoration(
-                      filled: false,
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.zero,
-                      hintText: 'Add Payment Method',
-                      hintStyle: TextStyle(
-                        fontSize: appTextTheme.bodyLarge?.fontSize,
-                        color: Theme.of(context).colorScheme.appText,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+    return BlocListener<OrdersBloc, OrdersState>(
+      listener: (context, state) {
+        if (state is PlaceOrderSuccess) {
+          OrderSuccessfulPageRoute().go(context);
+        }
+
+        if (state is OrdersError) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(state.message)),
+          );
+        }
+      },
+
+      child: AppScaffold(
+        appbar: AppBar(
+          title: AppText(
+            'Checkout',
+            style: appAltTextTheme.bodyMedium?.copyWith(
+              fontWeight: FontWeight.w800,
+              color: Theme.of(context).colorScheme.appText,
             ),
           ),
-          const Spacer(),
-          _buildPlaceOrderButton(),
-        ],
+          centerTitle: true,
+          forceMaterialTransparency: true,
+          leading: const AppBackButton(),
+        ),
+        body: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  // Shipping Address
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.bgColor,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: AppTextField(
+                      title: 'Shipping Address',
+                      verticalSpacing: 0,
+                      controller: _addressController,
+                      inputDecoration: InputDecoration(
+                        filled: false,
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.zero,
+                        hintText: 'Add Shipping Address',
+                        hintStyle: TextStyle(
+                          fontSize: appTextTheme.bodyLarge?.fontSize,
+                          color: Theme.of(context).colorScheme.appText,
+                        ),
+                      ),
+                    ),
+                  ),
+                  16.verticalSpacing,
+                  // Payment Method
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.bgColor,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: AppTextField(
+                      title: 'Payment Method',
+                      controller: _phoneController,
+                      verticalSpacing: 0,
+                      keyboardType: TextInputType.phone,
+                      inputDecoration: InputDecoration(
+                        filled: false,
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.zero,
+                        hintText: 'Add Payment Method',
+
+                        hintStyle: TextStyle(
+                          fontSize: appTextTheme.bodyLarge?.fontSize,
+                          color: Theme.of(context).colorScheme.appText,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(),
+            _buildPlaceOrderButton(),
+          ],
+        ),
       ),
     );
   }
@@ -108,21 +139,29 @@ class _CheckoutPageState extends State<CheckoutPage> {
               padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
             ),
             onPressed: () {
-              OrderSuccessfulPageRoute().go(context);
+              context.read<OrdersBloc>().add(
+                PlaceOrderEvent(
+                  shippingAddress: _addressController.text.trim(),
+                  shippingPhone: _phoneController.text.trim(),
+                  cartItems: state.cartItems,
+                  total: state.total,
+                ),
+              );
             },
             child: Row(
               children: [
                 AppText(
                   '\$${totalAmount.toStringAsFixed(2)}',
-                  style: appTextTheme.bodyLarge?.copyWith(
+                  style: appTextTheme.titleMedium?.copyWith(
                     color: AppColors.kWhite,
+                    fontWeight: FontWeight.w800,
                   ),
                 ),
                 const Spacer(),
 
                 AppText(
                   'Place Order',
-                  style: appTextTheme.bodyLarge?.copyWith(
+                  style: appTextTheme.titleMedium?.copyWith(
                     color: AppColors.kWhite,
                   ),
                 ),
