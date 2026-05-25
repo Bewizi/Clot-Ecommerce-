4610b60b7d991f6f47bd14b830fc1bb5464c1443
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2026-05-26 00:08:53 +0100

    feat: add OrdersBloc to BlocProvider for order management

--- lib/core/provider/bloc_provider.dart
+++ lib/core/provider/bloc_provider.dart
@@ -4,6 +4,8 @@ import 'package:clot/features/cart/data/cart_data.dart';
 import 'package:clot/features/cart/presentation/bloc/cart_bloc.dart';
 import 'package:clot/features/home/data/categories_data.dart';
 import 'package:clot/features/home/presentation/bloc/categories_bloc.dart';
+import 'package:clot/features/order/data/orders_data.dart';
+import 'package:clot/features/order/presentation/bloc/orders_bloc.dart';
 import 'package:clot/features/products/bloc/products_bloc.dart';
 import 'package:clot/features/products/data/products_data.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -21,5 +23,8 @@ final List<BlocProvider> appBlocProvider = [
   ),
   BlocProvider<CartBloc>(
     create: (context) => CartBloc(cartRepository: CartDataImpl()),
+  ),
+  BlocProvider<OrdersBloc>(
+    create: (context) => OrdersBloc(ordersRepository: OrdersDataImp()),
   ),
 ];
