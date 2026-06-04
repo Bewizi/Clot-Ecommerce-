4371bb039a8d82d466701f4220e5ac778795239f
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2026-06-04 08:06:49 +0100

    feat: integrate ProfileBloc and display user details in ProfileScreen
    - Trigger `LoadProfile` event in `initState` to fetch profile data.
    - Implement `BlocBuilder` to manage and display `ProfileLoading`, `ProfileLoaded`, and `ProfileError` states.
    - Display the user's full name and email address when the profile is successfully loaded.
    - Add a loading indicator and error message feedback to the UI.

--- lib/features/profile/presentation/pages/profile_screen.dart
+++ lib/features/profile/presentation/pages/profile_screen.dart
@@ -1,7 +1,9 @@
 import 'package:clot/core/ui/components/app_text.dart';
 import 'package:clot/core/ui/components/layouts/app_scaffold.dart';
 import 'package:clot/core/ui/extensions/app_theme_extension.dart';
+import 'package:clot/features/profile/presentation/bloc/profile_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -14,6 +16,12 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
+  void initState() {
+    super.initState();
+    context.read<ProfileBloc>().add(LoadProfile());
+  }
+
+  @override
   Widget build(BuildContext context) {
     return AppScaffold(
       body: Column(
@@ -23,6 +31,25 @@ class _ProfileScreenState extends State<ProfileScreen> {
               'Profile Page',
               style: context.textTheme.headlineLarge,
             ),
+          ),
+          BlocBuilder<ProfileBloc, ProfileState>(
+            builder: (context, state) {
+              if (state is ProfileLoading) {
+                return const CircularProgressIndicator();
+              } else if (state is ProfileLoaded) {
+                return Column(
+                  children: [
+                    AppText(
+                      'Full Name: ${state.profile.firstname} ${state.profile.lastname}',
+                    ),
+                    AppText('Email: ${state.profile.email}'),
+                  ],
+                );
+              } else if (state is ProfileError) {
+                return AppText('Error: ${state.message}');
+              }
+              return const SizedBox.shrink();
+            },
           ),
         ],
       ),
