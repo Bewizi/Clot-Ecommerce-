6f96064a91e50ba72a54dcc56e25fdf2074ef827
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2026-06-04 17:40:17 +0100

    Fix 'See All' route and make card responsive
    Wire up the "See All" gesture to the correct '/new-in' route (was an empty string). Replace the hardcoded product card width (150) with a responsive width using MediaQuery.sizeOf(context).width * 0.35 so cards scale better across screen sizes.

--- lib/features/products/presentation/pages/new_in.dart
+++ lib/features/products/presentation/pages/new_in.dart
@@ -44,7 +44,7 @@ class _NewInState extends State<NewIn> {
               ),
             ),
             GestureDetector(
-              onTap: () => context.push(''), // wire up your route
+              onTap: () => context.push('/new-in'), // wire up your route
               child: AppText(
                 'See All',
                 style: appTextTheme.bodyLarge!.copyWith(
@@ -116,7 +116,7 @@ class _ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 150,
+      width: MediaQuery.sizeOf(context).width * 0.35,
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.bgColor,
         borderRadius: BorderRadius.circular(AppRadius.medium),
